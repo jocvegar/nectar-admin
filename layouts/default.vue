818e6518d022d8e9638d87a4f0dfa28e26a029e6
Jose <jocvegar@gmail.com>
2021-01-02 11:13:52 -0600

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+
+      <v-spacer />
+
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="mt-4">
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-navigation-drawer v-model="rightDrawer" :right="true" temporary fixed>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title class="pija--text">HOLA HIJOS </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      items: [
+        {
+          icon: "mdi-home-variant",
+          title: "Home",
+          to: "/"
+        },
+        {
+          icon: "mdi-cup-water",
+          title: "Ventas",
+          to: "/ventas"
+        }
+      ],
+      miniVariant: true,
+      rightDrawer: false,
+      title: "NECTAR ADMIN"
+    };
+  }
+};
+</script>
---
+++ pages/ventas.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/templo_blanco.png"
+        alt="Vuetify.js"
+        class="mb-5"
+        style="height:200px;"
+      />
+      <blockquote class="blockquote">
+        HOLA GAYS
+        <footer>
+          <small>
+            <em>&mdash;Nectar</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
